5d9b58c4a0cadfdb65a674e0d447c8df79580358
Yehor40 <fraugrey3@gmail.com>
2022-10-12 11:23:41 +0200

    Crete table in jdbc and minor bug fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,7 +22,7 @@ public class Main {
     }}
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
        // System.out.println("Hello world!");
-            createT();
+            createTable();
 
 
         String query=" select * from JDBC ";
