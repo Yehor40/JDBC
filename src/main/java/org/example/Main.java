0e833207cb76335c64b038f0afa973c585fb2f27
Yehor40 <fraugrey3@gmail.com>
2022-10-12 11:20:46 +0200

    Crete table in jdbc and minor bug fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,50 @@
 package org.example;
-import java.io.*;
 import java.sql.*;
-
-
 public class Main {
+    static final String url="jdbc:mysql://localhost:3306/JDBC";
+    static final String user = "root";
+    static final String pass = "";
+    public static void createT(){
+        try(Connection con = DriverManager.getConnection(url, user, pass);
+            Statement st = con.createStatement();
+        ) {
+            String sql = "CREATE TABLE REGISTRATION " +
+                    "(id INTEGER not NULL, " +
+                    " first VARCHAR(255), " +
+                    " last VARCHAR(255), " +
+                    " age INTEGER, " +
+                    " PRIMARY KEY ( id ))";
+
+            st.executeUpdate(sql);
+            System.out.println("Created table in given database...");
+        } catch (SQLException e) {
+            e.printStackTrace();
+    }}
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
        // System.out.println("Hello world!");
-        String url="jdbc:mysql://localhost:3306/JDBC";
-        String user = "root";
-        String pass = "";
-        String query="select * from JDBC";
+            createT();
+
+
+        String query=" select * from JDBC ";
+
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         Connection con = DriverManager.getConnection(url,user,pass);
+
+
+
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(query);
         rs.next();
+
+
+        String id = rs.getString("id");
+
         String name = rs.getString("text");
-        System.out.println(name);
+        String num = rs.getString("integer");
+
+        System.out.println(id+"\t"+name+"\t"+num);
+
     st.close();
     con.close();}
 }
