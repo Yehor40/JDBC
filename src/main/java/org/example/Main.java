21e7e97bc510dae75f1ce6fd2720dc2a9bc8d6b9
Yehor40 <fraugrey3@gmail.com>
2022-10-13 15:45:21 +0200

    implementation of prepared statement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 import java.sql.*;
 import  java.util.*;
+// stored procedure:selectAll()
 
+/*
+-execute(): This method is used to execute SQL DDL statements, it returns a boolean value specifying whether the ResultSet object can be retrieved.
+
+-executeUpdate(): This method is used to execute statements such as insert, update, delete. It returns an integer value representing the number of rows affected.
+
+-executeQuery(): This method is used to execute statements that returns tabular data (example SELECT statement). It returns an object of the class ResultSet.
+*/
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static final String url="jdbc:mysql://localhost:3306/JDBC";
@@ -10,22 +18,55 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
        // System.out.println("Hello world!");
-        System.out.println("Choose 1 or 2:");
+        System.out.println("Choose:\n " +
+                "1:create \n " +
+                "2:select \n " +
+                "3:insert\n");
         int flag = sc.nextInt() ;
 
-           if(flag==1){
-               createTable();   }
-               else if(flag==2){
-                   select();
-                }
+        switch (flag){
+            case  1:
+                createTable();
+                break;
+            case  2:
+                select();
+                break;
+            case  3:
+                insert();
+                break;
+        }
+
+
+
+
 
        }
 
+    public static void call()throws ClassNotFoundException,SQLException {
+        //callable statement
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, user, pass);
+
+    }
+    public static void insert() throws ClassNotFoundException,SQLException {
+        //prepared statement
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, user, pass);
+
+      String query = "INSERT INTO JDBC(id, text,integr)VALUES(?, ?, ?)";
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1,1);
+            st.setString(2,"integer");
+            st.setInt(3,222);
+        st.executeUpdate();
+        System.out.println("inserted in table in given database...");
+        }
     public static void createTable(){
+        //statement
         try(Connection con = DriverManager.getConnection(url, user, pass);
             Statement st = con.createStatement()
         ) {
-            String sql = "CREATE TABLE REGISTRATION " +
+            String sql = "CREATE TABLE IF NOT EXISTS REGISTRATION " +
                     "(id INTEGER not NULL, " +
                     " first VARCHAR(255), " +
                     " last VARCHAR(255), " +
@@ -52,7 +93,7 @@ public class Main {
         String id = rs.getString("id");
 
         String name = rs.getString("text");
-        String num = rs.getString("integer");
+        String num = rs.getString("integr");
 
         System.out.println(id+"\t"+name+"\t"+num);
 
