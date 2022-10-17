1e17e54747867ef53613dfa2d93564c4d6a4b377
Yehor40 <fraugrey3@gmail.com>
2022-10-17 15:11:22 +0200

    final issue for Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,125 +1,133 @@
 package org.example;
 import java.sql.*;
 import  java.util.*;
+import java.util.concurrent.TimeUnit;
+
 // stored procedure:selectAll()
 
 /*
--execute(): This method is used to execute SQL DDL statements, it returns a boolean value specifying whether the ResultSet object can be retrieved.
+-Statement:
+The Statement interface represents the static SQL statement.
+It helps you to create a general purpose SQL statements using Java.
 
--executeUpdate(): This method is used to execute statements such as insert, update, delete. It returns an integer value representing the number of rows affected.
+-Prepared Statement
+The PreparedStatement interface extends the Statement interface.
+It represents a precompiled SQL statement which can be executed multiple times.
+This accepts parameterized SQL quires and you can pass 0 or more parameters to this query.
 
--executeQuery(): This method is used to execute statements that returns tabular data (example SELECT statement). It returns an object of the class ResultSet.
+-The CallableStatement
+Interface provides methods to execute stored procedures.
+Since the JDBC API provides a stored procedure SQL escape syntax,you can call stored procedures of all RDBMS in a single standard way.
 */
+
+
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    static final String url="jdbc:mysql://localhost:3306/JDBC";
+    static  Scanner sc = new Scanner(System.in);
+    static final String url = "jdbc:mysql://localhost:3306/JDBC";
     static final String user = "root";
     static final String pass = "";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-       // System.out.println("Hello world!");
+        // System.out.println("Hello world!");
         System.out.println("Choose:\n " +
                 "1:create \n " +
                 "2:select \n " +
-                "3:insert\n"+
-                 "4:call\n"
+                "3:insert\n" +
+                "4:call\n"
         );
-        int flag = sc.nextInt() ;
+        int flag = sc.nextInt();
 
-        switch (flag){
-            case  1:
+        switch (flag) {
+            case 1:
                 createTable();
                 break;
-            case  2:
+            case 2:
                 select();
                 break;
-            case  3:
+            case 3:
                 insert();
                 break;
-            case  4:
+            case 4:
                 call();
                 break;
         }
+    }
 
 
-
-
-
-       }
-
-    public static void call()throws ClassNotFoundException,SQLException {
-        //callable statement
+    public static void call() throws ClassNotFoundException, SQLException {
+        //callable statement to call stored procedure
+        long m = System.currentTimeMillis();
         String query = "{CALL selectAll()}";
         ResultSet rs;
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection(url, user, pass);
         CallableStatement st = con.prepareCall(query);
-        //
-        //
-        // st.setInt(1,1);
         rs = st.executeQuery();
-        rs.next();
-        String id = rs.getString("id");
-
-        String name = rs.getString("text");
-        String num = rs.getString("integr");
-
-        System.out.println(id+"\t"+name+"\t"+num);
+        while(rs.next()){
+            System.out.println(
+                            rs.getString("id")+"\t"+
+                            rs.getString("text")+"\t"+
+                            rs.getString("integr")
+           );}
         System.out.println("procedure called");
+        System.out.println(System.currentTimeMillis() - m);
         st.close();
         con.close();
-
     }
-    public static void insert() throws ClassNotFoundException,SQLException {
-        //prepared statement
+
+    public static void insert() throws ClassNotFoundException, SQLException {
+        //prepared statement to insert data into a table
+        long m = System.currentTimeMillis();
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection(url, user, pass);
-
-      String query = "INSERT INTO JDBC(id, text,integr)VALUES(?, ?, ?)";
-            PreparedStatement st = con.prepareStatement(query);
-            st.setInt(1,1);
-            st.setString(2,"integer");
-            st.setInt(3,222);
+        String query = "INSERT INTO JDBC(id, text,integr)VALUES(?, ?, ?)";
+        PreparedStatement st = con.prepareStatement(query);
+        st.setInt(1, 1);
+        st.setString(2, "integer");
+        st.setInt(3, 222);
         st.executeUpdate();
         System.out.println("inserted in table in given database...");
-        }
-    public static void createTable(){
-        //statement
-        try(Connection con = DriverManager.getConnection(url, user, pass);
-            Statement st = con.createStatement()
+        System.out.println(System.currentTimeMillis() - m);
+       // timeTrack();
+    }
+
+    public static void createTable() {
+        //simple statement to create table
+        long m = System.currentTimeMillis();
+        try (Connection con = DriverManager.getConnection(url, user, pass);
+             Statement st = con.createStatement()
         ) {
-            String sql = "CREATE TABLE IF NOT EXISTS REGISTRATION " +
+            String sql = "CREATE TABLE IF NOT EXISTS REGISTR " +
                     "(id INTEGER not NULL, " +
                     " first VARCHAR(255), " +
                     " last VARCHAR(255), " +
                     " age INTEGER, " +
                     " PRIMARY KEY ( id ))";
-
             st.executeUpdate(sql);
             System.out.println("Created table in given database...");
+            //timeTrack();
         } catch (SQLException e) {
             e.printStackTrace();
-        }}
+        }
+        System.out.println(System.currentTimeMillis() - m+"ms");
+    }
 
-    public static void select() throws ClassNotFoundException,SQLException {
-        String query=" select * from JDBC ";
-
+    public static void select() throws ClassNotFoundException, SQLException {
+        //simple statement to select all from table
+        long m = System.currentTimeMillis();
+        String query = " select * from JDBC ";
         Class.forName("com.mysql.cj.jdbc.Driver");
-
-        Connection con = DriverManager.getConnection(url,user,pass);
-
+        Connection con = DriverManager.getConnection(url, user, pass);
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(query);
         rs.next();
-
         String id = rs.getString("id");
-
         String name = rs.getString("text");
         String num = rs.getString("integr");
-
-        System.out.println(id+"\t"+name+"\t"+num);
-
+        System.out.println(id + "\t" + name + "\t" + num);
         st.close();
         con.close();
+        System.out.println(System.currentTimeMillis() - m+"ms");
+
     }
 }
